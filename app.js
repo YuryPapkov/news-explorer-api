756889ff8ba04140f9133d86eae97f1846dd7d73
Yury Papkov <yu-papkoff@yandex.ru>
2021-01-30 18:41:16 +0300

    added CORS blocking headers

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/newsdb' } = process.env;
 const app = express();
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger.js');
@@ -22,7 +22,15 @@ mongoose.connect(MONGO_URL, {
 app.use(bodyParser.json());
 app.use(requestLogger);
 app.use(limiter);
-app.use(helmet());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,append,delete,entries,foreach,get,has,keys,set,values,Authorization');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, HEAD, PUT, PATCH, POST, DELETE');
+  res.status(200);
+  next();
+});
+app.options('*', (req, res) => { res.status(200).send(); });
+// app.use(helmet());
 app.use('/api', router);
 app.use('*', () => {
   throw new NotFound('Запрашиваемый ресурс не найден');
